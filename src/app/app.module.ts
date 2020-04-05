8d7f32e0698a345d7daadb62ac1b41b43c3b2ad1
Carlos Rodriguez <carlos1843216@gmail.com>
2020-04-04 20:45:19 -0500

    Fin de la seccion 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
+//Servicios
+import { ServiceModule } from './services/service.module';
+
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -24,7 +27,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    FormsModule
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
+// ngFor, ngIf, pipes
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+
 
 @NgModule({
     declarations: [
@@ -17,6 +23,10 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
         SidebarComponent,
         NopagefoundComponent,
         BreadcrumbsComponent,
+    ],
+    imports:[
+        RouterModule,
+        CommonModule
     ]
 })
 
